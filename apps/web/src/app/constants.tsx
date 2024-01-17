30d1af724beb206fd86f94e8b9bca61d231aa5fe
ihsanhafidh <dhenmashafidh@gmail.com>
2024-01-17 23:33:27 +0700

    fix: fix feature landing

---
+++ apps/web/src/app/constants.tsx
@@ -0,0 +1,162 @@
+export const events = [
+  {
+    id: 1,
+    title: 'Event 1',
+    description: 'Description for Event 1',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 2,
+    title: 'Event 2',
+    description: 'Description for Event 2',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 3,
+    title: 'Event 3',
+    description: 'Description for Event 3',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+  {
+    id: 4,
+    title: 'Event 4',
+    description: 'Description for Event 4',
+    image:
+      'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/503/503', // Placeholder image URL
+    date: 'Fri, Feb 24 2024-02-10 12:30:32.239 +0700',
+    location: 'batam',
+    price: 20000,
+  },
+];
