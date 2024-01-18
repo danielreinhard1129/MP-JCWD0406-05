6512d675190254a8f2d8436a510edd67a7408992
Aldi Meirano <116731457+AldiMeirano@users.noreply.github.com>
2024-01-19 01:38:46 +0700

    feat: create a feature to protect routes after the user logs in (#7)

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,7 +1,13 @@
+'use client';
 import React from 'react';
 import LoginCard from './components/LoginCard';
-
+import { useAppSelector } from '@/lib/hooks';
+import { redirect } from 'next/navigation';
 const Register = () => {
+  const user = useAppSelector((state) => state.user);
+  if (user) {
+    redirect('/');
+  }
   return (
     <main className="container max-w-7xl px-4 mx-auto">
       <section className="mt-20 flex items-center justify-center md: mt-32">
--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import RegisterCard from "./components/RegisterCard";
+'use client';
+import React from 'react';
+import RegisterCard from './components/RegisterCard';
+import { useAppSelector } from '@/lib/hooks';
+import { redirect } from 'next/navigation';
 
-
-const Register = () => { 
-    return(
-        <main className="container max-w-7xl px-4 mx-auto"> 
-        <section className="mt-20 flex items-center justify-center md: mt-32">
-            <RegisterCard />
-        </section>
+const Register = () => {
+  const user = useAppSelector((state) => state.user);
+  if (user) {
+    redirect('/');
+  }
+  return (
+    <main className="container max-w-7xl px-4 mx-auto">
+      <section className="mt-20 flex items-center justify-center md: mt-32">
+        <RegisterCard />
+      </section>
     </main>
-     ) 
-}
+  );
+};
 
-export default Register
+export default Register;
